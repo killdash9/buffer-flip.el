386067141ca77848b266e068f8c6baf6b4c2ab5b
killdash9 <killdash9>
2016-08-11 08:13:39 -0600

    increment version

--- buffer-flip.el
+++ buffer-flip.el
@@ -5,7 +5,7 @@
 ;; Keywords: convenience
 ;; URL: https://github.com/killdash9/buffer-flip.el
 ;; Created: 10th November 2015
-;; Version: 1.1
+;; Version: 1.2
 ;; Package-Requires: ((key-chord "20150808"))
 
 ;; This program is free software; you can redistribute it and/or modify
